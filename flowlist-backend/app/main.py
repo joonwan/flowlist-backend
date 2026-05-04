6ad73b78faea74aec5d8e62436d8334ad9eeab21
joonwan1024 <joownan1024@naver.com>
2026-05-04 21:50:08 +0900

    feat: task id 기반 단건 조회 구현

--- flowlist-backend/app/main.py
+++ flowlist-backend/app/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from app.schemas.task import Task, TaskCreate
 
 app = FastAPI()
@@ -9,10 +9,12 @@ tasks: list[Task] = []
 def read_root():
     return {"message": "Flow List API is Running"}
 
+# task 전체 조회
 @app.get("/tasks")
 def get_tasks():
     return tasks
 
+# new task 등록
 @app.post("/tasks")
 def create_task(task_data: TaskCreate):
     new_task = Task(
@@ -23,3 +25,12 @@ def create_task(task_data: TaskCreate):
 
     tasks.append(new_task)
     return new_task
+
+# task 단건 조회 
+@app.get("/tasks/{task_id}")
+def get_task(task_id: int):
+    for task in tasks:
+        if task.id == task_id:
+            return task
+    
+    raise HTTPException(status_code=404, detail="Task not found")
