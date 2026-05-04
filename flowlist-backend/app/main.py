057a95d38ca33241f9a77624439ccd7154fea04f
joonwan1024 <joownan1024@naver.com>
2026-05-04 23:03:43 +0900

    feat: router prefix 적용

--- flowlist-backend/app/main.py
+++ flowlist-backend/app/main.py
@@ -1,58 +1,10 @@
 from fastapi import FastAPI, HTTPException
-from app.schemas.task import Task, TaskCreate, TaskUpdate
+from app.routers.tasks import router as tasks_router
 
 app = FastAPI()
-
-tasks: list[Task] = []
-
 
 @app.get("/")
 def read_root():
     return {"message": "Flow List API is Running"}
 
-
-# task 전체 조회
-@app.get("/tasks")
-def get_tasks():
-    return tasks
-
-
-# new task 등록
-@app.post("/tasks")
-def create_task(task_data: TaskCreate):
-    new_task = Task(id=len(tasks) + 1, title=task_data.title, is_done=False)
-
-    tasks.append(new_task)
-    return new_task
-
-
-# task 단건 조회
-@app.get("/tasks/{task_id}")
-def get_task(task_id: int):
-    for task in tasks:
-        if task.id == task_id:
-            return task
-
-    raise HTTPException(status_code=404, detail="Task not found")
-
-
-# task 수정
-@app.put("/tasks/{task_id}")
-def update_task(task_id: int, task_data: TaskUpdate):
-    for index, task in enumerate(tasks):
-        if task.id == task_id:
-            updated_task = Task(
-                id=task.id, title=task_data.title, is_done=task_data.is_done
-            )
-            tasks[index] = updated_task
-            return tasks[index]
-    raise HTTPException(status_code=404, detail="Task not found")
-
-
-# task 삭제
-@app.delete("/tasks/{task_id}")
-def delete_task(task_id: int):
-    for index, task in enumerate(tasks):
-        if task.id == task_id:
-            return tasks.pop(index)
-    raise HTTPException(status_code=404, detail="Task not found")
+app.include_router(tasks_router)
---
+++ flowlist-backend/app/routers/tasks.py
@@ -0,0 +1,52 @@
+from fastapi import APIRouter, HTTPException
+from app.schemas.task import Task, TaskCreate, TaskUpdate
+
+router = APIRouter(prefix="/tasks")
+
+tasks: list[Task] = []
+
+# task 전체 조회
+@router.get("")
+def get_tasks():
+    return tasks
+
+
+# new task 등록
+@router.post("")
+def create_task(task_data: TaskCreate):
+    new_task = Task(id=len(tasks) + 1, title=task_data.title, is_done=False)
+
+    tasks.append(new_task)
+    return new_task
+
+
+# task 단건 조회
+@router.get("/{task_id}")
+def get_task(task_id: int):
+    for task in tasks:
+        if task.id == task_id:
+            return task
+
+    raise HTTPException(status_code=404, detail="Task not found")
+
+
+# task 수정
+@router.put("/{task_id}")
+def update_task(task_id: int, task_data: TaskUpdate):
+    for index, task in enumerate(tasks):
+        if task.id == task_id:
+            updated_task = Task(
+                id=task.id, title=task_data.title, is_done=task_data.is_done
+            )
+            tasks[index] = updated_task
+            return tasks[index]
+    raise HTTPException(status_code=404, detail="Task not found")
+
+
+# task 삭제
+@router.delete("/{task_id}")
+def delete_task(task_id: int):
+    for index, task in enumerate(tasks):
+        if task.id == task_id:
+            return tasks.pop(index)
+    raise HTTPException(status_code=404, detail="Task not found")
