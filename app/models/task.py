44aa638368f6957bdfa03ee8992eb69d94b2f76b
joonwan1024 <joownan1024@naver.com>
2026-05-05 00:30:21 +0900

    feat: task entity 생성

---
+++ app/models/task.py
@@ -0,0 +1,12 @@
+from sqlalchemy import Boolean, Integer, stringprep
+from sqlalchemy.orm import Mapped, mapped_column
+
+from app.database import Base
+
+
+class Task(Base):
+    __tablename__ = "tasks"
+
+    id: Mapped[int] = mapped_column(Integer, primary_key=True, index=True)
+    title: Mapped[str] = mapped_column(String(255), nullable=False)
+    is_done: Mapped[bool] = mapped_column(Boolean, default=False, nullable=False)
