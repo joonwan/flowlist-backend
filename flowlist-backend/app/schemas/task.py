afee9df7a3ef988f18ec5a9f0a3d0493b72fcc47
joonwan1024 <joownan1024@naver.com>
2026-05-04 22:11:32 +0900

    feat: is_done update 가능하게 변경

--- flowlist-backend/app/schemas/task.py
+++ flowlist-backend/app/schemas/task.py
@@ -1,10 +1,16 @@
 from pydantic import BaseModel
 
+
 class TaskCreate(BaseModel):
     title: str
+
 
 class Task(BaseModel):
     id: int
     title: str
     is_done: bool
 
+
+class TaskUpdate(BaseModel):
+    title: str
+    is_done: bool
