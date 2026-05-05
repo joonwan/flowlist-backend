d13bf68219c7cd8e4414fde099503b816dae3277
joonwan1024 <joownan1024@naver.com>
2026-05-05 14:02:21 +0900

    chore: 환경변수 분리

--- app/database.py
+++ app/database.py
@@ -1,11 +1,17 @@
+import os
+
+from dotenv import load_dotenv
 from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base, sessionmaker
 
-DATABASE_URL = "mysql+pymysql://root:1234@localhost:3306/flowlist"
+
+load_dotenv()
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False)
 Base = declarative_base()
+
 
 def get_db():
     db = SessionLocal()
