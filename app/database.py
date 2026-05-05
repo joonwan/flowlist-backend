26ccdf2aff16e12b2feb301e6f1ab8a4e911475b
joonwan1024 <joownan1024@naver.com>
2026-05-05 14:10:46 +0900

    chore: db 환경변수 분리

--- app/database.py
+++ app/database.py
@@ -4,7 +4,6 @@ from dotenv import load_dotenv
 from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base, sessionmaker
 
-
 load_dotenv()
 DATABASE_URL = os.getenv("DATABASE_URL")
 
