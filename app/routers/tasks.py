6691a5be06ffa10b9597d1967b983b031214bc06
joonwan1024 <joownan1024@naver.com>
2026-05-05 13:56:28 +0900

    chore: 필요없는 HTTPException 의존성 삭제

--- app/routers/tasks.py
+++ app/routers/tasks.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Depends , Response, status
+from fastapi import APIRouter, Depends, Response, status
 from sqlalchemy.orm import Session
 
 from app.database import get_db
